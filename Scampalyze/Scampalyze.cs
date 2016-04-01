0555374c0f5d265dce3ab3f07b0d66dd6f516841
ljmorsillo <ljmorsillo@yahoo.com>
2016-04-01 10:21:40 -0400

    Refactored code to keep small methods Working

--- Scampalyze/Scampalyze.cs
+++ Scampalyze/Scampalyze.cs
@@ -47,33 +47,21 @@ namespace Ircda.Scampalyze
         }
         public override void Process()
         {
-            XmlDocument doc = new XmlDocument();
-            doc.Load(Path);
-           
-            // Get and display all the book titles.
-            XmlElement root = doc.DocumentElement;
-            int count = 0;
-            if (TagToCount.Equals(root.LocalName))
-            { count++; }
-            XmlNodeList elemList = root.GetElementsByTagName(TagToCount);
-            count += elemList.Count;
-            Console.WriteLine("Count of <{0}>: {1}", TagToCount, count);
-            //!!! Horrible POC (proof of concept or piece of c$&@ code...
-            DWCommand.Connection = DWConnection;
-            System.Data.SqlClient.SqlDataReader DWReader;
-            ///Inner text is not clear, change xpath...
-            string formname = root.SelectSingleNode("info/name").InnerText;
-            if (TagToCount.ToLower().Equals("element"))
+            try {
+                int formCount = countFromForm(Path, TagToCount);
+                ///!!! Inner text is not clear, change xpath...
+                string formname = getFormRoot(Path).SelectSingleNode("info/name").InnerText;
+                int DWcount = countFromDW(formname, TagToCount);
+                Console.WriteLine("Count of {0} -> Form: {1}, DW: {2}", TagToCount, formCount, DWcount);
+                Console.WriteLine("{0}", (formCount == DWcount) ? "Success" : "Failure");
+            }
+            catch (Exception exc)
             {
-                StringBuilder commandString = new StringBuilder();
-                    DWCommand.CommandText = commandString.AppendFormat("SELECT count(*) FROM vwFormElements WHERE (formname = '{0}');",formname).ToString();
-                DWConnection.Open();
-                //DWCommand.Connection.Open();
-                DWReader = DWCommand.ExecuteReader();
-                int columns = DWReader.FieldCount;
-                bool Ok = DWReader.Read();
-                int numForms = (int)DWReader.GetValue(0); //!!! horrible assumptions here
-                DWConnection.Close();
+                Console.WriteLine("Problem: {0}, please Fix: {1}.\nDetails:\n {2}", exc.Message, exc.Source, exc.ToString());
+            }
+            finally
+            {
+                Console.ReadKey();
             }
         }
         public override ARG_STAT Usage(string[] args)
@@ -110,6 +98,46 @@ namespace Ircda.Scampalyze
                 return ARG_STAT.ARGS_FAILED;
             }
             return ARG_STAT.ARGS_OK;
+        }
+
+        private int countFromForm(string path, string tagToCount)
+        {
+            XmlElement root = getFormRoot(path);
+            int count = 0;
+            if (tagToCount.Equals(root.LocalName))
+            { count++; }
+            XmlNodeList elemList = root.GetElementsByTagName(tagToCount);
+            count += elemList.Count;
+            return count;
+        }
+
+        private int countFromDW(string formname, string elementToCount)
+        {
+            int numForms = 0;
+            DWCommand.Connection = DWConnection;
+            if (TagToCount.ToLower().Equals("element"))
+            {
+                StringBuilder commandString = new StringBuilder();
+                DWCommand.CommandText = commandString.AppendFormat("SELECT count(*) FROM vwFormElements WHERE (formname = '{0}');", formname).ToString();
+                DWConnection.Open();
+                numForms = (int)DWCommand.ExecuteScalar();
+                DWConnection.Close();
+            }
+            return numForms;
+        }
+        /// <summary>
+        /// Utility to return the root element of the XML form document 
+        /// </summary>
+        /// <param name="path">Path to form document</param>
+        /// <returns cref="System.Xml.XmlElement>root XMLElement of document</returns>
+        /// <exception cref="System.IO.IOException"> Usual cast of bad file exceptions</exception>
+        /// <exception cref="System.Xml.XmlException">Usual cast of XML exceptions thrown</exception>
+        private XmlElement getFormRoot(string path)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(Path);
+            //if no root, exception thrown before now
+            return (doc.DocumentElement);
         }
     }
 }
